ea538611e1079ade48e61c306fe4a1aca7aa123c
Ritika-kashish <166491740+Ritika-kashish@users.noreply.github.com>
2024-05-18 02:51:33 +0530

    Add files via upload

---
+++ 01_face_dataset.py
@@ -0,0 +1,47 @@
+
+
+import cv2
+import os
+dataset_path = r'C:\Users\avimi\OneDrive\Desktop\New folder (5)\face recognition\dataset\user.'
+cam = cv2.VideoCapture(0)
+cam.set(3, 640) # set video width
+cam.set(4, 480) # set video height
+
+face_detector = cv2.CascadeClassifier(r'C:\Users\avimi\OneDrive\Desktop\New folder (5)\face detection\haarcascade_frontalface_default.xml')
+
+# For each person, enter one numeric face id
+face_id = input('\n enter user id end press <return> ==>  ')
+
+print("\n [INFO] Initializing face capture. Look the camera and wait ...")
+# Initialize individual sampling face count
+count = 0
+
+while(True):
+
+    ret, img = cam.read()
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+
+    for (x,y,w,h) in faces:
+
+        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
+        count += 1
+
+        # Save the captured image into the datasets folder
+        cv2.imwrite(dataset_path + str(face_id) + '.' + str(count) + ".jpg", img)
+
+        cv2.imshow('image', img)
+
+    k = cv2.waitKey(100) & 0xFF # Press 'ESC' for exiting video
+    if k == 27:
+        break
+    elif count >= 30: # Take 30 face sample and stop video
+         break
+
+# Do a bit of cleanup
+print("\n [INFO] Exiting Program and cleanup stuff")
+cam.release()
+cv2.destroyAllWindows()
+
+
